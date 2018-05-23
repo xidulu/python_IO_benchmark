dbbe99ca309bb492ff332e9cee4a4035dd5b5cc0
xidulu <xidulu@gmail.com>
2018-05-23 23:51:36 +0800

    multiple process communication done

--- main.py
+++ main.py
@@ -15,20 +15,49 @@ def write(blocksize, blockcount):
         count += 1
     os.fsync(out)
     time_elapsed = time.clock() - start
+    print ("average latency:{}ms").format((time_elapsed / count) * 1000)
     return count / time_elapsed
+
+
+def summerize(pid_list):
+    for pid in pid_list:
+        name = "./log/" + str(pid) + "stat.out"
+        log_in = open(name, "r")
+        print float(log_in.read())
 
 
 if __name__ == "__main__":
     blocksize = int(sys.argv[1]) * 1024
     blockcount = int(sys.argv[2])
+    processes = []
+    read_source = []
+
     if os.path.exists("./out"):
-        os.removedirs("./out")
+        os.system("rm -rf ./out")
     os.mkdir("./out")
+
+    if os.path.exists("./log"):
+        os.system("rm -rf ./log")
+    os.mkdir("./log")
     for i in range(process_num):
         t = os.fork()
         if t == 0:
-            #print os.getpid()
+            # Child process
+            name = str(os.getpid()) + "stat.out"
             result = write(blocksize, blockcount)
-            print ("{}Mb/s").format(result * blocksize/ 1024**2)
+            #print "{}Mb/s".format(result * blocksize/ 1024**2)
+            log_out = open("./log/" + name, "wb")
+            log_out.write(str(result * blocksize/ 1024**2))
+            log_out.close()
             os._exit(0)
+        else:
+            # Parent process
+            # read_source.append(r)
+            processes.append(t)
 
+    for pid_t in processes:
+        os.waitpid(pid_t, 0)
+
+    result = summerize(processes)
+    
+    
